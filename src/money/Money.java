f5d87c1c6ca9161e26278906996c487592832e3b
Y K <ky>
2017-12-18 21:51:50 +0900

    第10章終わり

--- src/money/Money.java
+++ src/money/Money.java
@@ -3,27 +3,36 @@ package money;
 /**
 
  */
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
-
-    abstract Money times(int multiplier);
 
     Money(int amount, String currency){
         this.amount = amount;
         this.currency = currency;
     }
 
+    public Money times (int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
     public boolean equals(Object object){
         Money money = (Money) object;
         return (
                 this.amount == money.amount &&
-                this.getClass() == money.getClass()
+                this.currency().equals(money.currency())
         );
     }
 
     public String currency(){
         return this.currency;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return this.amount + " " + this.currency;
     }
 
     static Money dollar(int amount){
