b28a61a1ae856b42a5402029ce3761da9e612300
Y K <ky>
2017-12-18 21:55:26 +0900

    第11章終わり

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -28,21 +28,9 @@ class MoneyTest {
     }
 
     @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        // assertEqualsメソッドは与えられたオブジェクト同士を.equals()メソッドを呼び出すことで比較する
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality(){
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-    }
 }
