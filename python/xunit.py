cf52f7b9b30ffd1fee98ad9c1ecb9c28cbf4c4de
Y K <ky>
2017-12-30 15:05:07 +0900

    第19章終わり

--- python/xunit.py
+++ python/xunit.py
@@ -4,26 +4,37 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
+    def setUp(self):
+        pass
+
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
 
     def testMethod(self):
         self.wasRun = 1
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
 
 class TestCaseTest(TestCase):
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+    def setUp(self):
+        self.test = WasRun("testMethod")
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
